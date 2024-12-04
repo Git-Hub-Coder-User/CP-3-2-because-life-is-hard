75575242c717408d9ba8f0d219be9fa858f33ead
Git-Hub-Coder-user <elsa.schultheiss@ucas-edu.net>
2024-12-04 11:21:32 -0700

    Started learning kivy

---
+++ kivy_test.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class HelloWorldApp(App):
+    def build(self):
+        return Label(text = "Hello World! ")
+    
+
+HelloWorldApp().run()
